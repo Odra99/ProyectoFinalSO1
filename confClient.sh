8f9c56cb7455e1ee9c456d19db555af61c0fd7b7
Fatima Odra Daniela Tezo Sum <fatimatezo201831039@cunoc.edu.gt>
2021-05-13 17:12:28 -0600

    Update confClient.sh

--- confClient.sh
+++ confClient.sh
@@ -1,8 +1,9 @@
 bacula-fd -tc /etc/bacula/bacula-fd.conf
 systemctl restart bacula-fd
 systemctl enable bacula-fd
-mkdir -p /bacula/restore
-chown -R bacula:bacula /bacula
-chmod -R 700 /bacula
+sudo mkdir -p /bacula/restore
+sudo chown -R bacula:bacula /bacula
+sudo chmod -R 700 /bacula
 
 firewall-cmd --zone=public --add-port=9102/tcp --permanent
+firewall-cmd --reload
